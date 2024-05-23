3390b2d020c3e1d65c6702197ec9edc995e63aa6
KM0912 <61630275+KM0912@users.noreply.github.com>
2024-05-23 22:14:20 +0900

    generate_toeic_question.pyを実行することで問題文が作成されるように変更

--- src/generate_toeic_question.py
+++ src/generate_toeic_question.py
@@ -1,5 +1,42 @@
+import os
 import openai
+import json
 import prompts
+from config import load_settings
+from output_dir_manager import OutputDirManager
+
+app_settings = load_settings("settings.toml").app
+
+
+def main():
+    questions = []
+    for i in range(10):
+        toeic_questions = generate_toeic_questions()
+        try:
+            toeic_questions = json.loads(toeic_questions)
+            questions = questions + toeic_questions["questions"]
+        except json.JSONDecodeError as e:
+            print("Error: Failed to parse JSON")
+            continue
+
+    print("Generated TOEIC Part 1 questions.")
+
+    # これまでに生成した問題数を取得
+    output_dir_manager = OutputDirManager()
+    question_count = output_dir_manager.max_output_dir_number()
+
+    # 問題文を保存
+    for i, question in enumerate(questions):
+        folder_name = os.path.join(
+            output_dir_manager.OUTPUT_DIR,
+            output_dir_manager.ACTIVE_DIR,
+            str(question_count + i + 1),
+        )
+        os.makedirs(folder_name, exist_ok=True)
+
+        json_file_path = os.path.join(folder_name, "question.json")
+        with open(json_file_path, "w") as f:
+            json.dump(question, f, indent=4)
 
 
 def generate_toeic_questions():
@@ -20,3 +57,7 @@ def generate_toeic_questions():
         temperature=0.5,
     )
     return response.choices[0].message.content
+
+
+if __name__ == "__main__":
+    main()
